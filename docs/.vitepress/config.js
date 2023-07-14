5002f79ccb1f8a16e040ff040b1d7be2c226537b
在同一时空相遇 <y17870181601@163.com>
2023-07-14 19:09:11 +0800

    配置部署base路径

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'parseAplayerComponents',
   description: '使用自定义组件快速配置APlayer播放器',
+  base: '/parse-aplayer-components/',
   themeConfig: {
     socialLinks: [
       { icon: 'github', link: 'https://github.com/yuannancheng/parse-aplayer-components' }
